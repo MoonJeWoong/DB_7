e922de2fcac0352a99f4dc13a7940ab31444ea52
to-besomeone <evillion99@gmail.com>
2018-12-06 18:19:04 +0900

    직원관리 - 직원 삭제 화면 변경 - (삭제기능 -> 삭제확인메세지)

--- src/HotelManage/deleteStaffInform.java
+++ src/HotelManage/deleteStaffInform.java
@@ -3,58 +3,37 @@ package HotelManage;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class deleteStaffInform implements ActionListener {
     private JFrame frame = new JFrame("직원정보 삭제");
     private JPanel mainPanel = new JPanel();
-    private JLabel staffID = new JLabel("직원 ID");
-    private JTextField staffIDInput = new JTextField();
-    private JButton deleteButton = new JButton("삭제");
-    private Connection dbTest;
+    private JLabel alert = new JLabel("삭제되었습니다.");
+    private JButton okButton= new JButton("확인");
 
-    public deleteStaffInform(Connection dbTest){
-        this.dbTest = dbTest;
+    public deleteStaffInform(){
         prepareGUI();
     }
 
     private void prepareGUI(){
-        frame.setSize(300, 150);
+        frame.setSize(190, 150);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
         mainPanel.setLayout(null);
-        staffID.setBounds(25, 20, 70, 30);
-        staffIDInput.setBounds(100, 20, 150, 30);
-        deleteButton.setBounds(180, 60, 70, 30);
+        alert.setBounds(40, 10, 150, 30);
+        okButton.setBounds(55, 50, 70, 30);
+        okButton.addActionListener(this);
 
-        deleteButton.addActionListener(this);
-
-        mainPanel.add(staffID);
-        mainPanel.add(staffIDInput);
-        mainPanel.add(deleteButton);
+        mainPanel.add(alert);
+        mainPanel.add(okButton);
 
         frame.add(mainPanel);
         frame.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e){
-        if(e.getSource() == deleteButton){
-            try{
-                String delquery = "delete from Staff where StaffId = "+staffIDInput.getText();
-                PreparedStatement stmt = dbTest.prepareStatement(delquery);
-                ResultSet rs = stmt.executeQuery();
-
-                frame.dispose();
-
-                rs.close();
-                stmt.close();
-            } catch (SQLException se){
-                se.printStackTrace();
-            }
+        if(e.getSource() ==okButton){
+            frame.dispose();
         }
     }
 }
